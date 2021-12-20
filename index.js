fcc1e897016ab3e253c2948209e2fef661d8b10e
Felcoj22 <est.juan.correa7@unimilitar.edu.co>
2021-12-20 16:43:00 -0500

    Coloca Variables en consola

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-var nombre = "felipe";
+//Variable numerica
+var num = 22;
+var dec = 22.2;
+
+var operacion = num + dec;
+
+/////////////////////////////////////////////
+
+//Cadena de texto
+var text = "Hola, estoy aprendiendo JS";
+
+/////////////////////////////////////////////
+
+//Booleano
+if (operacion > 0) {
+    console.log('Positivo');
+}
+else {
+    console.log('Negativo');
+}
+
+//////////////////////////////////////////////
+
+//HTML
+
+
+/////////////////////////////////////////////
+
+//Arreglos
+var arr = ['Nom', "Apell", num];
+
+//////////////////////////////////////////////
+
+//Objetos
+var obj = {
+    Nom: 'Duster',
+    Color: 'Rojo',
+    Tipo: 'Camioneta',
+    Cant: num
+}
+
+//////////////////////////////////////////////
+
+//Funciones
+var func = Function();
+
 
 //alert(nombre);
-//console.log(nombre)
+console.log(operacion)
+console.log(text)
+console.log(arr)
+console.log(obj)
